5dc59f6c6cf0bc8d55ae217ffb7ea86f595ec5ba
Hatice Koçak <kocakkhatice@gmail.com>
2021-01-21 20:15:40 +0300

    connect to server

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+const app = require("express")();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
+
+
+app.get("/", (req, res) => {
+    res.end("odev 4 server");
+
+});
+//connection listening 
+io.on("connection", (socket) => {
+    /*//send response to client
+   socket.emit("welcome" )*/
+
+   socket.broadcast.emit("new-user", {title: "a user joined", joinDate: new Date()})
+
+   socket.on("disconnect", () => console.log("a user disconnected"));
+});
+
+http.listen(4000, () => {
+    console.log("listening at 4000");
+})
